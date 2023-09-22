fe31793934f2b4c9563fe21de375008049b0a543
Ernest <leyefoer@gmail.com>
2023-09-22 10:57:21 +0800

    added todo icon at header and edit html tags

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,11 +1,18 @@
 import classes from "./Header.module.css";
 import todoImage from "../assets/image.jpg";
+import TodoHeaderIcon from "../assets/TodoHeader.svg";
 
 export default function Header() {
   return (
     <>
       <div>
         <nav className={classes.header}>
+          <img
+            src={TodoHeaderIcon}
+            alt="ToDo Header Icon"
+            width="40"
+            height="40"
+          />
           <h1 className={classes.title}>TodoList</h1>
         </nav>
 
