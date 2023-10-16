2c30d85b5068f9d434ef370edd57b06b16504dd2
Ernest <leyefoer@gmail.com>
2023-10-16 21:39:42 +0800

    config vercel

--- api/index.js
+++ api/index.js
@@ -9,6 +9,10 @@ require("dotenv/config");
 
 const app = express();
 app.use(express.static(path.join(__dirname, "../build")));
+const path = require("path");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
