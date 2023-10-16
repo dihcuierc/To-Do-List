7fe32381f7df386d8d7fe473fde98c6bfab9cea4
Ernest <leyefoer@gmail.com>
2023-10-16 21:48:54 +0800

    update api/index.js

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ require("dotenv/config");
 
 const app = express();
 app.use(express.static(path.join(__dirname, "../build")));
-const path = require("path");
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
 });
